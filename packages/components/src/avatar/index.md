8541980a820a9dd1ee03482fe66a66bb44fb611e
nqsjd178559344 <1401444513@qq.com>
2021-05-18 13:17:41 +0800

    feat: Avatar demo1

--- packages/components/src/avatar/index.md
+++ packages/components/src/avatar/index.md
@@ -16,22 +16,16 @@ nav:
 
 ```tsx
 import React from 'react';
-// import { Avatar } from '@cz160/components';
+import { Avatar } from '@cz160/components';
 
 export default () => {
-  // return (
-  //   <>
-  //     <Button onClick={e => console.log(e)}>默认按钮</Button>
-  //     <br />
-  //     <br />
-  //     <Button type="primary">高亮按钮</Button>
-  //     <br />
-  //     <br />
-  //     <Button type="primary" disabled>
-  //       我被禁用
-  //     </Button>
-  //   </>
-  // );
+  return (
+    <>
+      <Avatar shape="square" size="64">
+        1
+      </Avatar>
+    </>
+  );
 };
 ```
 
@@ -46,5 +40,3 @@ export default () => {
 | srcSet    | 设置图片类头像响应式资源地址                                  | `string`                                   | -       |
 | alt       | 图像无法显示时的替代文本                                      | `string`                                   | -       |
 | loadError | 图片加载失败的事件，返回 false 会关闭组件默认的 fallback 行为 | `() => boolean`                            | -       |
-
-
--- packages/components/src/avatar/index.tsx
+++ packages/components/src/avatar/index.tsx
@@ -1,47 +1,25 @@
 import React from 'react';
 import cx from 'classnames';
-import { useCallback } from 'react';
 
 export type AvatarShapes = 'shape' | 'circle';
 export type AvatarSize = 'large' | 'small' | 'default';
 
 export interface AvatarProps {
   shape?: AvatarShapes;
-  size: AvatarSize;
-  // prefixCls?: string;
-  onClick?: (e: any) => void;
-  disabled?: boolean;
+  size?: AvatarSize;
+  prefixCls?: string;
 }
-const Avatar: React.FC<AvatarProps> = props => {
-  const {
-    shape = 'circle',
-    size = 'default',
-    disabled = false,
-    children,
-    // prefixCls = 'cz-btn',
-    onClick,
-  } = props;
 
-  // 处理点击事件
-  const handleClick = useCallback(
-    e => {
-      if (onClick) {
-        onClick(e);
-      }
-    },
-    [onClick],
-  );
+const Avatar: React.FC<AvatarProps> = props => {
+  const { shape = 'circle', size = 'default', prefixCls = 'cz-avatar' } = props;
 
   return (
-    // <button
-    //   className={cx(prefixCls, {
-    //     [`${prefixCls}-${type}`]: type,
-    //   })}
-    //   onClick={handleClick}
-    //   disabled={disabled}
-    // >
-    //   {children}
-    // </button>
+    <span
+      className={cx(prefixCls, {
+        [`${prefixCls}-${shape}`]: shape,
+        [`${prefixCls}-${size}`]: size,
+      })}
+    ></span>
   );
 };
 
